8edb83ca52a3771713adb32b5303fbda21270b80
Ian Odhiambo <you@example.com>
2018-08-25 14:31:11 +0300

    save multiple test done

--- tests.py
+++ tests.py
@@ -14,7 +14,12 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user = User('ian', 'Odhiambo', 'ianodad', '0712724144', 'ianodad@gmail.com', 'Pythoncode1')
 
-    
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+
     def test_init(self):
         '''
         intiating test for users
@@ -29,11 +34,19 @@ class TestUser(unittest.TestCase):
     
     def test_add_user(self):
         '''
-        test_add test case to test if the contact object is saved into
-         the contact list
+        test_add test case for adding new conatcts
         '''
         self.new_user.add_user()  # saving the new contact
         self.assertEqual(len(User.user_list), 1)
+
+    def test_save_multiple_users(self):
+            '''
+            test_save_multiple_user to save multiple contacts
+            '''
+            self.new_user.add_user()
+            new_user_test = User('John', 'Doe', 'johndoe', '0712000000', 'johndoe@gmail.com', 'johndoe254')
+            new_user_test.add_user()
+            self.assertEqual(len(User.user_list), 2)
 
 
 if __name__ == '__main__':
