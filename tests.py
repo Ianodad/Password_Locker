687c9ff203fa362894f3ccf2f6f3c3b18310a536
Ian Odhiambo <you@example.com>
2018-08-25 13:00:10 +0300

    adding user works wit test

--- tests.py
+++ tests.py
@@ -1,22 +1,40 @@
 '''
 we begin by import unittest and User form user.py
 '''
-
-from user import User
 import unittest
+from user import User
 
-class Tests(unittest.TestCase):
+
+class TestUser(unittest.TestCase):
 
     def setUp(self):
 
         '''
         set up method
         '''
+        self.new_user = User('ian', 'Odhiambo', 'ianodad', '0712724144', 'ianodad@gmail.com', 'Pythoncode1')
+
+    
     def test_init(self):
         '''
-        int
+        intiating test for users
+
         '''
+        self.assertEqual(self.new_user.first_name, "ian")
+        self.assertEqual(self.new_user.last_name, "Odhiambo")
+        self.assertEqual(self.new_user.username, "ianodad")
+        self.assertEqual(self.new_user.phone_number, "0712724144")
+        self.assertEqual(self.new_user.email, "ianodad@gmail.com")
+        self.assertEqual(self.new_user.password, "Pythoncode1")
+    
+    def test_add_user(self):
+        '''
+        test_add test case to test if the contact object is saved into
+         the contact list
+        '''
+        self.new_user.add_user()  # saving the new contact
+        self.assertEqual(len(User.user_list), 1)
 
-    new_user = User('ian', 'Odhiambo', 'ianodad','0712724144', 'ianodad@gmail.com')
 
-
+if __name__ == '__main__':
+    unittest.main()
--- user.py
+++ user.py
@@ -1,15 +1,27 @@
 class User:
 
-    def __init__(self, first_name, last_name, username, phone_number: int, email):
+    user_list = []
+
+    def __init__(self, first_name, last_name, username, phone_number, email, password):
 
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
         self.phone_number = phone_number
         self.email = email
+        self.password = password
+
+    def add_user(self):
+
+        '''
+        add new user
+
+        '''
+        User.user_list.append(self)
+  
+
+# new_user = User('ian', 'Odhiambo', 'ianodad',
+#                 '0712724144', 'ianodad@gmail.com', 'Pytoncode1')
 
 
-new_user = User('ian', 'Odhiambo', 'ianodad',
-                '0712724144', 'ianodad@gmail.com')
 
-print(new_user.first_name)
